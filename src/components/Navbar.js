ef573dac8b6d090cf8d377ec1fb7e45b005c0feb
john-chang94 <j.chang94@yahoo.com>
2022-08-12 13:23:05 -0700

    test ref

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
-import $ from "jquery";
 
 export default function Navbar() {
-	$('.primary-nav').css('height', $('.logo').height());
-    $('.primary-nav li').css(
-      'margin-top',
-      ($('.primary-nav').height() - $('.primary-nav li').height()) / 2 + 'px'
-    );
-  
-    // $(window).resize(function () {
-    //   setTimeout(navigation, 500);
-    // });
+  const logoRef = useRef();
+
+  useEffect(() => {
+    console.log(logoRef.current.clientHeight);
+  }, []);
 
   return (
     <div className="navigation">
       <div className="container-fluid">
         <div className="row">
           <div className="col-xs-1 col-sm-1 col-md-1 col-lg-1 text-center">
-            <div className="logo">
-              <a href="index.html">
+            <div className="logo" ref={logoRef}>
+              <Link to="/">
                 <img src="images/logo.svg" />
-              </a>
+              </Link>
             </div>
           </div>
 
           <div className="col-xs-11 col-sm-11 col-md-11 col-lg-11 text-right">
             <div className="primary-nav">
               <ul>
-                <Link to="/">
-                  <a href="index.html">Home</a>
-                </Link>
-                <Link to="/portfolio">
-                  <a href="portfolio.html">Portfolio</a>
-                </Link>
-                <Link to="/contact">
-                  <a href="contact.html">Contact</a>
-                </Link>
+                <Link to="/">Home</Link>
+                <Link to="/portfolio">Portfolio</Link>
+                <Link to="/contact">Contact</Link>
               </ul>
             </div>
           </div>
